bcb29d42e8dbb7d3feca7e85c47e0be49a76d97c
Bartek Pacia <barpac02@gmail.com>
2019-08-09 10:59:17 +0200

    added task 4.2 from the excercise book

---
+++ excercise_book/task_4_2.py
@@ -0,0 +1,10 @@
+# Write a program that puts
+# numbers from 9 to 0 in a
+# 10-element array
+
+array = []
+for i in range(9, -1, -1):
+    array.append(i)
+
+for number in array:
+    print(number)
