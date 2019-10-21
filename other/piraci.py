251f94bd413c736c5c1db854c25d9bce0a16527a
Bartek Pacia <barpac02@gmail.com>
2019-10-21 17:25:40 +0200

    piraci.py - done example a)

--- other/piraci.py
+++ other/piraci.py
@@ -1,8 +1,11 @@
 from datetime import date
+from datetime import datetime
+import datetime
 dni = 150
 
 poczatek = date(1902, 10, 1)
 wigilia = date(1902, 12, 24)
+dzisiaj = poczatek
 
 roznica = wigilia - poczatek
 print(roznica)
@@ -13,20 +16,29 @@ dystans = 0  # całkowita przebyta odległość
 dublony = 0
 
 for dzien in range(1, dni + 1):
-    coords["x"] += 8
-    coords["y"] += 11
+    nowe_dublony = 0
+    # dodatkowe dublony każdego trzeciego dnia miesiąca
+    if dzisiaj.day == 3:  # wychodzi na to, że dni są o jeden zaniżone
+        nowe_dublony += 2
+        print("Znaleziono dodatkowe 2 dublony!")
+
+    coords["y"] += 8
+    coords["x"] += 11
     mile_na_polnoc += 8
     dystans += (8 + 11)
 
-    wykopane_dublony = len(str(mile_na_polnoc))
+    nowe_dublony += len(str(mile_na_polnoc))
 
-    dublony += wykopane_dublony
-    coords["x"] -= wykopane_dublony
-    dystans += wykopane_dublony
+    dublony += nowe_dublony
+    coords["x"] -= nowe_dublony
+    dystans += nowe_dublony
 
-    print("- - - - - - - - - - - - - - - -")
-    print("Dzień " + str(dzien))
-    print("Przebyty dystans: " + str(dystans))
-    print("Nowe dublony: " + str(wykopane_dublony))
+    print("Dzień " + str(dzien) + " | " + str(dzisiaj))
+    print("Przebyty dystans: " + str(dystans) +
+          ", na północ: " + str(mile_na_polnoc))
+    print("Nowe dublony: " + str(nowe_dublony))
     print("Dublony łącznie: " + str(dublony))
     print("Obóz w x: " + str(coords["x"]) + "  y: " + str(coords["y"]))
+    print("- - - - - - - - - - - - - - - -")
+
+    dzisiaj = dzisiaj + datetime.timedelta(1)
