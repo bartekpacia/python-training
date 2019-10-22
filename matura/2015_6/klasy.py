ced93343f3ff641e02950c4fd28d07d73c28a7f7
Bartek Pacia <barpac02@gmail.com>
2019-10-22 11:58:08 +0200

    moved klasy.py to separate file

---
+++ matura/2015_6/klasy.py
@@ -0,0 +1,29 @@
+class Kierowca:
+    def __init__(self, id, nazwisko, imie, kraj):
+        self.id = id
+        self.nazwisko = nazwisko
+        self.imie = imie
+        self.kraj = kraj
+
+    def __str__(self):
+        return "Kierowca id: " + self.id + ", nazwisko: " + self.nazwisko + ", imie: " + self.imie + ", kraj: " + self.kraj
+
+
+class Wyscig:
+    def __init__(self, id, rok, miejsce):
+        self.id = id
+        self.rok = rok
+        self.miejsce = miejsce
+
+    def __str__(self):
+        return "Wyscig id: " + self.id + ", rok: " + self.rok + ", miejsce: " + self.miejsce
+
+
+class Wynik:
+    def __init__(self, id_kierowcy, punkty, id_wyscigu):
+        self.id_kierowcy = id_kierowcy
+        self.punkty = punkty
+        self.id_wyscigu = id_wyscigu
+
+    def __str__(self):
+        return "Wynik id_kierowcy: " + self.id_kierowcy + ", punkty: " + str(self.punkty) + ", id_wyscigu:" + self.id_wyscigu
--- matura/2015_6/program.py
+++ matura/2015_6/program.py
@@ -1,37 +1,7 @@
 '''https://cke.gov.pl/images/_EGZAMIN_MATURALNY_OD_2015/Arkusze_egzaminacyjne/2015/formula_od_2015/MIN-R2_1P-152.pdf
 zadanie 6'''
 
-
-class Kierowca:
-    def __init__(self, id, nazwisko, imie, kraj):
-        self.id = id
-        self.nazwisko = nazwisko
-        self.imie = imie
-        self.kraj = kraj
-
-    def __str__(self):
-        return "Kierowca id: " + self.id + ", nazwisko: " + self.nazwisko + ", imie: " + self.imie + ", kraj: " + self.kraj
-
-
-class Wyscig:
-    def __init__(self, id, rok, miejsce):
-        self.id = id
-        self.rok = rok
-        self.miejsce = miejsce
-
-    def __str__(self):
-        return "Wyscig id: " + self.id + ", rok: " + self.rok + ", miejsce: " + self.miejsce
-
-
-class Wynik:
-    def __init__(self, id_kierowcy, punkty, id_wyscigu):
-        self.id_kierowcy = id_kierowcy
-        self.punkty = punkty
-        self.id_wyscigu = id_wyscigu
-
-    def __str__(self):
-        return "Wynik id_kierowcy: " + self.id_kierowcy + ", punkty: " + str(self.punkty) + ", id_wyscigu:" + self.id_wyscigu
-
+from klasy import Kierowca, Wyscig, Wynik
 
 kierowcy = list()
 wyscigi = list()
