ffd732d8c5720ba575d9f8647ebe263c8e9a048c
Bartek Pacia <barpac02@gmail.com>
2020-03-18 19:22:01 +0100

    added dijkstra

--- algorithms/8_dijkstra.py
+++ algorithms/8_dijkstra.py
@@ -24,4 +24,29 @@ parents["meta"] = None
 
 processed = []
 
-print("Let's go!")
+# Beginning of the actual implementation
+
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    print(f"lowest cost node: {node}")
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
