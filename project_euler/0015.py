8df333225055f6514351004e2037268b18564b86
Bartek Pacia <barpac02@gmail.com>
2020-03-31 12:33:26 +0200

    done 15th

--- project_euler/0015.py
+++ project_euler/0015.py
@@ -1,14 +1,14 @@
-graph = {}
-graph[0] = 0
+grid_size = int(input("Podaj rozmiar siatki: "))
 
-new_graph_num = 1
+grid = [[0 for x in range(grid_size + 1)] for x in range(grid_size + 1)]
 
+# fill 2 boundaries with 1s
+for i in range(grid_size + 1):
+    grid[0][i] = 1
+    grid[i][0] = 1
 
-def gen(size):
-    for i in range(1, size*size):
-        print(i)
-        graph[i] = new_graph_num
-        new_graph_num += 1
+for i in range(1, grid_size + 1):
+    for j in range(1, grid_size + 1):
+        grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
 
-
-gen(4)
+print(f"Liczba ścieżek: {grid[grid_size][grid_size]}")
