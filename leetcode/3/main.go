00a1047f70ab44cc5afed8619eb4eac0e406f3e3
Bartek Pacia <barpac02@gmail.com>
2020-11-25 00:10:10 +0100

    add leetcodes done in Go

---
+++ leetcode/3/main.go
@@ -0,0 +1,27 @@
+func lengthOfLongestSubstring(s string) int {
+    
+    longest := 0
+    for i, _ := range s {
+        chars := make(map[int32]bool)
+        
+        currentLongest := 0
+        for _, char := range s[i:] {
+            exists, _ := chars[char]
+            
+            if exists {
+                break
+            } else {
+                chars[char] = true
+            }
+            
+            currentLongest++
+        }
+        
+        if currentLongest > longest {
+            longest = currentLongest
+        }
+    }
+    
+    return longest
+}
+
